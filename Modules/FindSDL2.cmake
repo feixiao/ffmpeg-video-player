270f06d1d2a825a1cc5148d1540a44ffeaf9f09a
feixiao <feixiao2020@sina.com>
2022-01-11 16:27:30 +0800

    解决OSX上SDL2库引用不对问题

--- Modules/FindSDL2.cmake
+++ Modules/FindSDL2.cmake
@@ -1,15 +1,3 @@
-
-SET(SDL2_SEARCH_PATHS
-~/Library/Frameworks
-/Library/Frameworks
-/usr/local
-/usr
-/sw # Fink
-/opt/local # DarwinPorts
-/opt/csw # Blastwave
-/opt
-)
-
 ##
 # Find SDL2 Libraries.
 #
@@ -18,4 +6,10 @@ SET(SDL2_SEARCH_PATHS
 #   SDL2_INCLUDE_DIRS - Include directory necessary for using the required components headers.
 #   SDL2_LIBRARIES    - Link these to use the required ffmpeg components.
 ##
+
+# find_library(SDL2_LIBRARIES SDL2)
+
 PKG_SEARCH_MODULE(SDL2 REQUIRED sdl2)
+if (CMAKE_SYSTEM_NAME MATCHES "Darwin")
+    set(SDL2_LIBRARIES /usr/local/Cellar/sdl2/2.0.12_1/lib/libSDL2.dylib)
+endif ()
